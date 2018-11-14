6a0380d73893c180767f755f500f2deb64c6cde3
Carlos Gomes <carlos.gomes@thomsonreuters.com>
2018-11-14 10:48:16 -0200

    changed server to include ingredients on recipes

--- server/endpoints/recipes/recipes.routes.ts
+++ server/endpoints/recipes/recipes.routes.ts
@@ -1,5 +1,13 @@
 import { app } from '../../app';
+import * as INGREDIENTS from '../ingredients/ingredients.json';
 import * as RECIPES from './recipes.json';
+
+RECIPES.forEach((recipe: any) => {
+  recipe.ingredientsList = recipe.ingredientsIds.map((id: number) =>
+    INGREDIENTS.find((ingredient) => ingredient.id === id),
+  );
+  recipe.ingredientsList.forEach((ingredient: any) => (ingredient.amount = 1));
+});
 
 app.get('/api/recipes', (req, res) => {
   res.json(RECIPES);
