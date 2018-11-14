22a98aab0780c6eb17ec5d0359c3659dd8b15400
Carlos Gomes <carlos.gomes@thomsonreuters.com>
2018-11-14 13:41:02 -0200

    Fix recipes endpoint changing original ingredient

--- server/endpoints/recipes/recipes.routes.ts
+++ server/endpoints/recipes/recipes.routes.ts
@@ -3,10 +3,9 @@ import * as INGREDIENTS from '../ingredients/ingredients.json';
 import * as RECIPES from './recipes.json';
 
 RECIPES.forEach((recipe: any) => {
-  recipe.ingredientsList = recipe.ingredientsIds.map((id: number) =>
-    INGREDIENTS.find((ingredient) => ingredient.id === id),
-  );
-  recipe.ingredientsList.forEach((ingredient: any) => (ingredient.amount = 1));
+  recipe.ingredientsList = recipe.ingredientsIds
+    .map((id: number) => INGREDIENTS.find((ingredient) => ingredient.id === id))
+    .map((ingredient: any) => ({ ...ingredient, amount: 1 }));
 });
 
 app.get('/api/recipes', (req, res) => {
